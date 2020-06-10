5daa0e896dec80b82c3a002e5854ad8062c97a46
Esteban Castano <esteban.crw@gmail.com>
2020-06-09 22:22:13 -0500

    feat(scraper): add google cloud functions-framework

--- scraper/src/index.js
+++ scraper/src/index.js
@@ -1,36 +1,52 @@
-const { take } = require('ramda')
+const { flatten, take } = require('ramda')
 const CrawlerAggregate = require('./crawler')
-const data = require('../data.json')
+const dataFile = require('../data.json')
 const { generateListings } = require('./property')
 const log = require('./logger')
 const { getWebsites, parseData } = require('./parse-data')
 
-const run = async () => {
-  const listingData = parseData(data)
-  const listings = generateListings(listingData)
+// takeOne :: a[] -> [a]
+const takeOne = take(1)
 
-  const partialListings = take(1, listings)
+// propertiesFromListing :: (crawlerAggregate, listing) -> Promise<property[]>
+const propertiesFromListing = async (crawlerAggregate, listing) => {
+  const links = await crawlerAggregate.getLinks(listing)
 
-  const websites = getWebsites(data)
-  const crawlerAggregate = CrawlerAggregate(websites)
+  // temporary for testing a single link
+  const partialLinks = takeOne(links)
+  const properties = await crawlerAggregate.getProperties(partialLinks)
 
-  await Promise.all(
-    partialListings.map(async (listing) => {
-      const links = await crawlerAggregate.getLinks(listing)
-
-      const partialLinks = take(1, links)
-
-      const properties = await crawlerAggregate.getProperties(partialLinks)
-
-      log.info('run: properties', properties)
-    }),
-  )
+  return properties
 }
 
-run()
-  .then(() => {
-    log.info('run: completed execution')
-  })
-  .catch((error) => {
+// propertiesFromListings :: (crawlerAggregate, listing[]) -> Promise<property[]>
+const propertiesFromListings = async (crawlerAggregate, listings) => {
+  const nestedProperties = await Promise.all(
+    listings.map((listing) => propertiesFromListing(crawlerAggregate, listing)),
+  )
+
+  return flatten(nestedProperties)
+}
+
+// Background Cloud Function
+exports.scraper = async (data, context, callback) => {
+  const listingData = parseData(dataFile)
+  const listings = generateListings(listingData)
+
+  // temporary for testing a single listing
+  const partialListings = takeOne(listings)
+
+  const websites = getWebsites(dataFile)
+  const crawlerAggregate = CrawlerAggregate(websites)
+
+  let properties
+  try {
+    properties = await propertiesFromListings(crawlerAggregate, partialListings)
+  } catch (error) {
     log.error(error)
-  })
+    callback(error)
+  }
+
+  log.info('run: properties', properties)
+  callback()
+}
