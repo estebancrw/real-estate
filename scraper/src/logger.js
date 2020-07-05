0dc3e44b76bec33078da5fbf4f6770fd8a0e4087
Esteban Castano <esteban.crw@gmail.com>
2020-07-04 19:21:19 -0500

    fix(scraper): default logger name typo

--- scraper/src/logger.js
+++ scraper/src/logger.js
@@ -1,7 +1,7 @@
 const bunyan = require('bunyan')
 
 const level = process.env.LOG_LEVEL || 'info'
-const name = process.env.FUNCTION_TARGET || 'scaper'
+const name = process.env.FUNCTION_TARGET || 'scraper'
 
 const log = bunyan.createLogger({
   level,
