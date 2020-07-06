3afce6206846e84bf163a3c5b832abb163acf4dd
Esteban Castano <esteban.crw@gmail.com>
2020-07-05 19:27:20 -0500

    refactor(scraper): function composition for browser code

--- scraper/src/websites/lamudi/page-function.js
+++ scraper/src/websites/lamudi/page-function.js
@@ -1,67 +1,62 @@
 /* eslint-env browser */
 
 const pageFn = (property) => {
+  const safePipe = (...fns) => (prop) => {
+    return fns.reduce((value, fn) => {
+      if (value === null) {
+        return null
+      }
+
+      return fn(value)
+    }, prop)
+  }
+  const map = (fn) => (list) => list.map(fn)
+
+  const queryDocument = (selector) => () => document.querySelector(selector)
+  const queryAllDocument = (selector) => () =>
+    Array.from(document.querySelectorAll(selector))
+  const queryElement = (selector) => (element) =>
+    element.querySelector(selector)
+  const parentElement = (element) => element.parentElement
+  const nextSiblingElement = (element) => element.nextElementSibling
+  const textContent = (element) => element.textContent
+  const href = (element) => element.href
+
   // linksFn :: void -> string[]
-  const linksFn = () => {
-    const selector = '.ListingCell-moreInfo-button-v2_redesign'
-    const elements = Array.from(document.querySelectorAll(selector))
-
-    if (elements === null) {
-      return null
-    }
-
-    const hrefs = elements.map((element) => element.href)
-
-    return hrefs
-  }
-
-  // priceFn :: void -> string
-  const priceFn = () => {
-    const selector = '.FirstPrice'
-    const element = document.querySelector(selector)
-
-    if (element === null) {
-      return null
-    }
-
-    return element.textContent
-  }
+  const linksFn = safePipe(
+    queryAllDocument('.ListingCell-moreInfo-button-v2_redesign'),
+    map(href),
+  )
 
   // bathroomsFn :: void -> string
-  const bathroomsFn = () => {
-    const selector = 'div > i.icon-bathrooms'
-    const element = document.querySelector(selector)
+  const bathroomsFn = safePipe(
+    queryDocument('#listing-details'),
+    queryElement('.icon-bathrooms'),
+    parentElement,
+    nextSiblingElement,
+    textContent,
+  )
 
-    if (element === null) {
-      return null
-    }
-
-    return element.nextElementSibling.textContent
-  }
+  // priceFn :: void -> string
+  const priceFn = safePipe(queryDocument('.FirstPrice'), textContent)
 
   // bedroomsFn :: void -> string
-  const bedroomsFn = () => {
-    const selector = 'div > i.icon-bedrooms'
-    const element = document.querySelector(selector)
-
-    if (element === null) {
-      return null
-    }
-
-    return element.nextElementSibling.textContent
-  }
+  const bedroomsFn = safePipe(
+    queryDocument('#listing-details'),
+    queryElement('.icon-bedrooms'),
+    parentElement,
+    nextSiblingElement,
+    textContent,
+  )
 
   // buildingSizeFn :: void -> string
-  const buildingSizeFn = () => {
-    const selector = 'div > i.icon-livingsize'
-    const element = document.querySelector(selector)
-
-    if (element === null) {
-      return null
-    }
-
-    return element.nextElementSibling.textContent
-  }
+  const buildingSizeFn = safePipe(
+    queryDocument('#listing-details'),
+    queryElement('.icon-livingsize'),
+    parentElement,
+    nextSiblingElement,
+    textContent,
+  )
 
   // apartment :: void -> object
   const apartment = () => {
