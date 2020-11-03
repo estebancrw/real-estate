5f92a4782a0b7bdb79578d63fb4b07263084fa85
Esteban Castano <esteban.crw@gmail.com>
2020-11-03 11:15:26 -0600

    feat(property): keeps only property service

--- scraper/src/property/property-service.js
+++ scraper/src/property/property-service.js
@@ -1,45 +1,8 @@
-const { flatten } = require('ramda')
-const linkRepository = require('./link-repository')
-const log = require('../logger')
-const propertyRepository = require('./property-repository')
-
-function PropertyService(crawlerAggregate) {
-  const crawler = crawlerAggregate
-
-  // linksFromListing :: listing -> Promise<link[]>
-  const linksFromListing = async (listing) => {
-    log.info('property-service: get links from listing', listing)
-    const links = await crawler.getLinks(listing)
-
-    return linkRepository.filterNonVisited(links)
-  }
-
-  // linksFromListings :: listing[] -> Promise<link[]>
-  const linksFromListings = async (listings) => {
-    const linksList = await Promise.all(
-      listings.map((listing) => linksFromListing(listing)),
-    )
-
-    return flatten(linksList)
-  }
-
-  // propertiesFromLinks :: link[] -> Promise<property[]>
-  const propertiesFromLinks = (links) => crawler.getProperties(links)
-
-  // commit :: (link[], property[]) -> Promise<void>
-  const commit = async (links, properties) => {
-    log.info('property-service: store properties', properties)
-    await propertyRepository.addAll(properties)
-
-    log.info('property-service: store links', links)
-    await linkRepository.addAll(links)
-  }
+function PropertyService(website) {
+  const fetchProperty = (listing, url) => website.fetchProperty(listing, url)
 
   return {
-    commit,
-    propertiesFromLinks,
-    linksFromListing,
-    linksFromListings,
+    fetchProperty,
   }
 }
 
